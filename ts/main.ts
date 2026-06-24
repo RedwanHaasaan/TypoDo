47ca637069d57045a493131e68e1fbc307611e40
Redwan Hasan <redwanhasan.workspace@gmail.com>
2026-06-24 20:56:18 +0600

    TD-003: Implement UI part and Todo Types

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,8 @@
-console.log("TypeScript Connected");
+import { Todo } from "./types.js";
+//array to store todos
+let todos: Todo[] = [];
+
+//select elements
+const form=document.querySelector<HTMLFormElement>('#todo-form');
+const input=document.querySelector<HTMLInputElement>('#todo-input');
+const todoList=document.querySelector<HTMLUListElement>('#todo-list');
